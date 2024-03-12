49260dfbdb9d9404d3b3e7d2ebf3949719d53a11
Shihab585203 <freelancersehab1@gmail.com>
2024-03-12 16:29:13 +0600

    initial setup and mongodb connect

---
+++ index.js
@@ -0,0 +1,56 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+require('dotenv').config()
+
+
+//Middleware
+app.use(express.json());
+app.use(cors());
+
+//db username: foodCab
+// db password: foodCab112233
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.xlp2yoh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+console.log(process.env.DB_USER, process.env.DB_PASSWORD);
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    const productsCollection = client.db("foodCab").collection("foodItems");
+
+    app.get("/products", async (req, res) => {
+        const query = {};
+        const cursor = productsCollection.find(query);
+        const products = await cursor.toArray();
+        res.send(products);
+        console.log(products);
+    })
+   
+  } finally {
+  }
+}
+run().catch(error => console.log(error));
+
+
+
+app.get("/", (req, res) => {
+    res.send("Food Cab Server is Running Successfully!")
+});
+
+app.listen(port, () => {
+    console.log(`This server is running on Port ${port}`);
+});
